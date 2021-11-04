8031f99b3d31df0b1fbf14d87b734ce7fbfec33f
94lorena <ivette.navas.mt@correo.usa.edu.co>
2021-11-04 15:38:09 -0500

    first commit

--- js/misFunciones.js
+++ js/misFunciones.js
@@ -7,6 +7,96 @@
 /**
  * endpoint api audience Oracle CLoud 
  */
+//////////////////////Reservaciones//////////////////////////////////        
+
+function traerInformacionStatus(){
+    console.log("test");
+    $.ajax({
+        url:"http://132.226.242.207:8081/api/Reservation/report-status",
+        type:"GET",
+        datatype:"JSON",
+        success:function(respuesta){
+            console.log(respuesta);
+            pintarRespuesta(respuesta);
+        }
+    });
+}
+function pintarRespuesta(respuesta){
+
+    let myTable="<table>";
+    myTable+="<tr>";
+       myTable+="<th>completadas</th>";
+        myTable+="<td>"+respuesta.completed+"</td>";
+        myTable+="<th>canceladas</th>";
+        myTable+="<td>"+respuesta.cancelled+"</td>";
+        myTable+="</tr>";
+    myTable+="</table>";
+    $("#resultadoStatus").html(myTable);
+}
+function traerInformacionReporteDate(){
+
+    var fechaInicio = document.getElementById("RstarDate").value;
+    var fechaCierre = document.getElementById("RdevolutionDate").value;
+    console.log(fechaInicio);
+    console.log(fechaCierre);
+    
+        $.ajax({
+            url:"http://132.226.242.207:8081/api/Reservation/report-dates/"+fechaInicio+"/"+fechaCierre,
+            type:"GET",
+            datatype:"JSON",
+            success:function(respuesta){
+                console.log(respuesta);
+                pintarRespuestaDate(respuesta);
+            }
+        });
+    }
+    function pintarRespuestaDate(respuesta){
+
+        let myTable="<table>";
+        myTable+="<tr>";
+          
+        for(i=0;i<respuesta.length;i++){
+        myTable+="<th>total</th>";
+            myTable+="<td>"+respuesta[i].devolutionDate+"</td>";
+            myTable+="<td>"+respuesta[i].startDate+"</td>";
+            myTable+="<td>"+respuesta[i].status+"</td>";
+          
+          
+            myTable+="</tr>";
+        }
+        myTable+="</table>";
+        $("#resultadoDate").html(myTable);
+    }
+
+    function traerInformacionReporteClientes(){
+        $.ajax({
+            url:"http://132.226.242.207:8081/api/Reservation/report-clients",
+            type:"GET",
+            datatype:"JSON",
+            success:function(respuesta){
+                console.log(respuesta);
+                pintarRespuestaClientes(respuesta);
+            }
+        });
+    }
+    function pintarRespuestaClientes(respuesta){
+
+        let myTable="<table>";
+        myTable+="<tr>";
+          
+        for(i=0;i<respuesta.length;i++){
+        myTable+="<th>total</th>";
+            myTable+="<td>"+respuesta[i].total+"</td>";
+            myTable+="<td>"+respuesta[i].client.name+"</td>";
+            myTable+="<td>"+respuesta[i].client.email+"</td>";
+            myTable+="<td>"+respuesta[i].client.age+"</td>";
+          
+            myTable+="</tr>";
+        }
+        myTable+="</table>";
+        $("#resultadoClientes").html(myTable);
+    }
+
 
 //////////////////////Categoria//////////////////////////////////
 
@@ -658,99 +748,6 @@ function borrarInformacionReservacion(idReservation){
         });
 
 
-//////////////////////Reservaciones//////////////////////////////////        
 
-function traerInformacionStatus(){
-    $.ajax({
-        url:"http://132.226.242.207:80811/api/report-status",
-        type:"GET",
-        datatype:"JSON",
-        success:function(respuesta){
-            console.log(respuesta);
-            pintarRespuestaStatus(respuesta);
-        }
-    });
-}
-
-function pintarRespuestaStatus(respuesta){
-
-    let myTable="<table>";
-    myTable+="<tr>";
-       myTable+="<th>completadas</th>";
-        myTable+="<td>"+respuesta.completed+"</td>";
-        myTable+="<th>canceladas</th>";
-        myTable+="<td>"+respuesta.cancelled+"</td>";
-        myTable+="</tr>";
-    myTable+="</table>";
-    $("#resultadoStatus").html(myTable);
-}
-
-function traerInformacionReporteDate(){
-    
-   
-
-    var fechaInicio = document.getElementById("RstarDate").value;
-    var fechaCierre = document.getElementById("RdevolutionDate").value;
-  console.log(fechaInicio);
-  console.log(fechaCierre);
-
-    $.ajax({
-        url:"http://132.226.242.207:8081/api/Reservation/report-dates/"+fechaInicio+"/"+fechaCierre,
-        type:"GET",
-        datatype:"JSON",
-        success:function(respuesta){
-            console.log(respuesta);
-            pintarRespuestaDate(respuesta);
-        }
-    });
-}
-function pintarRespuestaDate(respuesta){
-
-    let myTable="<table>";
-    myTable+="<tr>";
-      
-    for(i=0;i<respuesta.length;i++){
-    myTable+="<th>total</th>";
-        myTable+="<td>"+respuesta[i].devolutionDate+"</td>";
-        myTable+="<td>"+respuesta[i].startDate+"</td>";
-        myTable+="<td>"+respuesta[i].status+"</td>";
-      
-      
-        myTable+="</tr>";
-    }
-    myTable+="</table>";
-    $("#resultadoDate").html(myTable);
-}
-
-
-function traerInformacionReporteClientes(){
-    $.ajax({
-        url:"http://132.226.242.207:8081/api/Reservation/report-clients",
-        type:"GET",
-        datatype:"JSON",
-        success:function(respuesta){
-            console.log(respuesta);
-            pintarRespuestaClientes(respuesta);
-        }
-    });
-}
-
-function pintarRespuestaClientes(respuesta){
-
-    let myTable="<table>";
-    myTable+="<tr>";
-      
-    for(i=0;i<respuesta.length;i++){
-    myTable+="<th>total</th>";
-        myTable+="<td>"+respuesta[i].total+"</td>";
-        myTable+="<td>"+respuesta[i].client.name+"</td>";
-        myTable+="<td>"+respuesta[i].client.email+"</td>";
-        myTable+="<td>"+respuesta[i].client.age+"</td>";
-      
-        myTable+="</tr>";
-    }
-    myTable+="</table>";
-    $("#resultadoClientes").html(myTable);
-}
 
 }
